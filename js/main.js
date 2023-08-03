96ba6b52e0a0bbb1f243fc1e01cc68cdf5ef11ab
Neil_Nguyen <higherblood2309@gmail.com>
2023-08-03 11:04:14 +0700

    fixx

--- js/main.js
+++ js/main.js
@@ -1,140 +1,110 @@
-document.querySelector("#btnSVCaoDiemNhat").onclick = function () {
-  var arrTagDiem = document.querySelectorAll("#tblBody td:nth-child(4)");
-  console.log(arrTagDiem);
+let btnDiem = document.querySelector('#btnSVCaoDiemNhat');
 
-  var arrDiem = [];
+btnDiem.onclick = function () {
+  let arrTagDiem = document.querySelectorAll('#tblBody td:nth-child(4)');
 
-  for (var index = 0; index < arrTagDiem.length; index++) {
-    var diem = Number(arrTagDiem[index].innerHTML);
+  let arrDiem = [];
+  for (let index = 0; index < arrTagDiem.length; index++) {
+    let diem = arrTagDiem[index].innerHTML;
+    console.log(diem)
+    arrDiem.push(diem);
+  }
+  let arrTagName = document.querySelectorAll('#tblBody td:nth-child(3)');
 
+  let arrName = [];
+  for (let iName = 0; iName < arrTagName.length; iName++) {
+    let name = arrTagName[iName].innerHTML;
+
+    arrName.push(name);
+  }
+
+  let output = '';
+  let indexMax = findIndexMax(arrDiem);
+  output = 'sinh viên có điểm cao nhất ' + arrName[indexMax] + ' -  số điểm ' + arrDiem[indexMax];
+  document.getElementById('svGioiNhat').innerHTML = output;
+}
+
+let btnDiemMin = document.querySelector('#btnSVThapDiemNhat');
+btnDiemMin.onclick = function () {
+  let arrDiem = document.querySelectorAll('#tblBody td:nth-child(4)');
+  let arrTagDiem = [];
+
+  for (let index = 0; index < arrDiem.length; index++) {
+    let diem = arrDiem[index].innerHTML;
+    arrTagDiem.push(diem);
+  }
+
+  let arrTen = document.querySelectorAll('#tblBody td:nth-child(3)');
+  let arrTagName = [];
+
+  for (let index = 0; index < arrTen.length; index++) {
+    let name = arrTen[index].innerHTML;
+    arrTagName.push(name)
+  }
+
+  let output = '';
+  let tagMin = findIndexMin(arrTagDiem);
+  output = 'sinh viên thấp điểm nhất  ' + arrTagName[tagMin] + ' số điểm là ' + arrTagDiem[tagMin];
+  document.getElementById('svYeuNhat').innerHTML = output;
+
+}
+//
+let btnSVGioi = document.querySelector('#btnSoSVGioi');
+btnSVGioi.onclick = function () {
+  let arrTagDiem = document.querySelectorAll('#tblBody td:nth-child(4)');
+
+  let arrDiem = [];
+  for (let index = 0; index < arrTagDiem.length; index++) {
+    let diem = arrTagDiem[index].innerHTML;
+    console.log(diem)
     arrDiem.push(diem);
   }
 
-  var arrTagName = document.querySelectorAll("#tblBody td:nth-child(3)");
-  var arrName = [];
+  let output = 0;
+  output = diemSVGioi(arrDiem, 8);
+  document.getElementById('soSVGioi').innerHTML = output;
 
-  for (var index = 0; index < arrTagName.length; index++) {
-    var Name = arrTagName[index].innerHTML;
+}
+//
+let btnSVHon5 = document.querySelector('#btnSVDiemHon5');
+btnSVHon5.onclick = function () {
+  let arrTagDiem = document.querySelectorAll('#tblBody td:nth-child(4)');
 
-    arrName.push(Name);
-  }
-  console.log(arrName);
-  console.log(arrDiem);
-  //output: nguyễn văn a - 9 điểm: output: string
-
-  var output = "";
-  
-
-  var indexMax = finIndexMax(arrDiem);
-
-  output =
-    "sinh viên có điểm cao nhất: " +
-    arrName[indexMax] +
-    " - " +
-    arrDiem[indexMax];
-
-  document.querySelector("#svGioiNhat").innerHTML = output;
-};
-
-document.querySelector("#btnSVThapDiemNhat").onclick = function () {
-  var arrTagDiem = document.querySelectorAll("#tblBody td:nth-child(4)");
-  console.log(arrTagDiem);
-
-  var arrDiem = [];
-
-  for (var index = 0; index < arrTagDiem.length; index++) {
-    var diem = Number(arrTagDiem[index].innerHTML);
-
+  let arrDiem = [];
+  for (let index = 0; index < arrTagDiem.length; index++) {
+    let diem = arrTagDiem[index].innerHTML;
     arrDiem.push(diem);
   }
 
-  var arrTagName = document.querySelectorAll("#tblBody td:nth-child(3)");
-  var arrName = [];
-
-  for (var index = 0; index < arrTagName.length; index++) {
-    var Name = arrTagName[index].innerHTML;
-
-    arrName.push(Name);
+  let arrTagName = document.querySelectorAll('#tblBody td:nth-child(3)');
+  let arrName = [];
+  for (let iName = 0; iName < arrTagName.length; iName++) {
+    let name = arrTagName[iName].innerHTML;
+    arrName.push(name);
   }
-  console.log(arrName);
-  console.log(arrDiem);
 
-  var ketQua = "";
-  var indexMin = finIndexMin(arrDiem);
+  let output = '';
+  output = nameSVLonHon5(arrName,arrDiem);
 
-  ketQua =
-    "Sinh viên có điểm thấp nhất: " +
-    arrName[indexMin] +
-    " - " +
-    arrDiem[indexMin];
+  document.getElementById('dsDiemHon5').innerHTML = output;
 
-  document.querySelector("#svYeuNhat").innerHTML = ketQua;
-};
-// điếm số lượng sv giỏi
-document.querySelector("#btnSoSVGioi").onclick = function () {
-  //input: arrdiem []
-  var arrTagDiem = document.querySelectorAll("#tblBody td:nth-child(4)");
-  console.log(arrTagDiem);
+}
+//
+let btnSort = document.querySelector('#btnSapXepTang');
+btnSort.onclick = function () {
+  let arrTagDiem = document.querySelectorAll('#tblBody td:nth-child(4)');
 
-  var arrDiem = [];
-  for (var index = 0; index < arrTagDiem.length; index++) {
-    var diem = Number(arrTagDiem[index].innerHTML);
-
+  let arrDiem = [];
+  for (let index = 0; index < arrTagDiem.length; index++) {
+    let diem = arrTagDiem[index].innerHTML;
     arrDiem.push(diem);
   }
-  //output: soLuongSVGioi:number
-  var soLuongSVGioi = 0;
-  soLuongSVGioi = demSinhViengioi(arrDiem, 8);
-  //in ouput ra giao diện
 
-  document.querySelector("#soSVGioi").innerHTML = soLuongSVGioi;
-};
+  let output = 0 ;
+  output = sortMax(arrDiem);
 
-document.querySelector("#btnSVDiemHon5").onclick = function () {
-  var arrTagDiem = document.querySelectorAll("#tblBody td:nth-child(4)");
-  console.log(arrTagDiem);
-
-  var arrDiem = [];
-  for (var index = 0; index < arrTagDiem.length; index++) {
-    var diem = Number(arrTagDiem[index].innerHTML);
-
-    arrDiem.push(diem);
-  }
-  var arrTagName = document.querySelectorAll("#tblBody td:nth-child(3)");
-  var arrName = [];
-
-  for (var index = 0; index < arrTagName.length; index++) {
-    var Name = arrTagName[index].innerHTML;
-
-    arrName.push(Name);
-  }
-
-  //oupuut: html tên các sv trên 5 : string
-
-  var html = "";
-
-  html = diemTBHon5(arrDiem, arrName);
-
-  document.querySelector("#dsDiemHon5").innerHTML = html;
-};
-
-document.querySelector("#btnSapXepTang").onclick = function () {
-  var arrTagDiem = document.querySelectorAll("#tblBody td:nth-child(4)");
-  console.log(arrTagDiem);
-
-  var arrDiem = [];
-  for (var index = 0; index < arrTagDiem.length; index++) {
-    var diem = Number(arrTagDiem[index].innerHTML);
-
-    arrDiem.push(diem);
-  }
-  //output: arrDiemSapxep
-
-  var arrDiemSX = [];
-  arrDiemSX = sapXepTangDanSo(arrDiem);
-
-  document.querySelector("#dtbTang").innerHTML = "Điểm: " + arrDiemSX;
-};
+  document.getElementById('dtbTang').innerHTML = output;
+}
 
 
 
